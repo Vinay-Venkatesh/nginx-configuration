7800e0b6f6ab7e67200fbf53d549a60e3812c891
Vinay <36667412+xxCoDeBoX@users.noreply.github.com>
2020-07-19 17:06:37 +0530

    Commenting filesystem mount op
    Commenting filesystem mount op

--- nginx_configuration/manifests/linux_configuration.pp
+++ nginx_configuration/manifests/linux_configuration.pp
@@ -7,31 +7,31 @@
 class nginx_configuration::linux_configuration{
 
   #Create mount location and add a record in /etc/fstab
-  $hash      = loadjson("/etc/puppetlabs/code/environment/production/modules/nginx_configuration/files/mount.json")
-  $id        = $hash['filesystem_id']
-  $mounts    = $hash['mount_points']
+  #$hash      = loadjson("/etc/puppetlabs/code/environment/production/modules/nginx_configuration/files/mount.json")
+  #$id        = $hash['filesystem_id']
+  #$mounts    = $hash['mount_points']
 
-  $id.each |$index, $value|{
-    $filesystem_id = $id[$index]
-    $mount_loc     = $mounts[$index]
+  #$id.each |$index, $value|{
+    #$filesystem_id = $id[$index]
+    #$mount_loc     = $mounts[$index]
 
-    file{"${mount_loc}":
-      ensure => 'directory',
-      owner  => 'root',
-      mode   => '0755',
-    }->
+    #file{"${mount_loc}":
+    #  ensure => 'directory',
+    #  owner  => 'root',
+    #  mode   => '0755',
+    #}->
 
-    file_line{"update /etc/fstab-${index}":
-      path   => '/etc/fstab',
-      line   => "${filesystem_id} ${mount_loc} xfs  defaults,nofail  0  2"
-    }
-  }
+    #file_line{"update /etc/fstab-${index}":
+    #  path   => '/etc/fstab',
+    #  line   => "${filesystem_id} ${mount_loc} xfs  defaults,nofail  0  2"
+    #}
+  #}
 
   #Mount volumes
-  exec{'mount_volumes':
-    path        => '/usr/bin',
-    command     => 'mount -a'
-  }
+  #exec{'mount_volumes':
+    #path        => '/usr/bin',
+    #command     => 'mount -a'
+  #}
 
   #Copy custom index.html file to ebs volume
   exec{'copy index.html':
